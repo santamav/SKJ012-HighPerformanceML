dd3f3a3635303f1078954e4eae3caca54dcad6b8
vicentamen <santamav@uji.es>
2024-02-01 17:59:19 +0100

    Ejercicio 1 y 2

--- 2_2_Codigos_Iniciales/pi_par_vector.c
+++ 2_2_Codigos_Iniciales/pi_par_vector.c
@@ -33,14 +33,14 @@ int main(int argc, char *argv[]) {
   // Sequential implementation
   //
   double x, sum = 0.0;
-  t1 = ....
+  t1 = omp_get_wtime();
   step = 1.0 / (double) num_steps;  
   for (i=0; i<num_steps; i++){
      x = (i+0.5)*step;
      sum = sum + 4.0/(1.0+x*x);
   }
   pi = step * sum;
-  t2 = ...
+  t2 = omp_get_wtime();
   t_seq = (t2-t1);
 
   printf(" pi_seq = %20.15f\n", pi);
@@ -52,30 +52,43 @@ int main(int argc, char *argv[]) {
   
   // Defining the size of the vector
   int size = 0;
-#pragma omp parallel default(none) shared(...)
+#pragma omp parallel default(none) shared(size)
   {
-    size = ...
+    size = omp_get_num_threads();
   }
   double pi_vector[size];
 
   // Computation of pi
-  t1 = ...
+  t1 = omp_get_wtime();
 #pragma omp parallel default(none) \
-                     shared(...)
+                     shared(pi_vector, num_steps, step)
   {
-    int rank = ...
-    int size = ...
+    int rank = omp_get_thread_num();
+    int size = omp_get_num_threads();
 
     // Accumulating on pi_vector[rank] the computation of each thread
     // ...
+    pi_vector[rank] = 0.0;
+    #pragma omp for
+    for (int i=0; i<num_steps; i++){
+//    for (int i=rank; i<num_steps; i+=size){
+      double x = (i+0.5)*step;
+      pi_vector[rank] += 4.0/(1.0+x*x);
+    }
   }
 
   // Accummulate the values on pi_vector to pi
   // ...
-  t2 = ...
-  t_par = ...
-  sp = ...
-  ep = ...
+  sum = 0.0;
+  for (i=0; i<size; i++){
+    sum += pi_vector[i];
+  }
+  pi = step * sum;
+
+  t2 = omp_get_wtime();
+  t_par = t2-t1; // Compute the time from start to end of the thread
+  //sp = ...
+  //ep = ...
 
   printf(" pi_par = %20.15f\n", pi);
   printf(" time_par = %20.15f, Sp = %20.15f , Ep = %20.15f\n", t_par, sp, ep);
